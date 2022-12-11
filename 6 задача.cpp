6fc2e3f943319c6ed5372c2e32d546385b3eebab
chervyachiha <113230399+chervyachiha@users.noreply.github.com>
2022-12-11 19:04:24 +0200

    Add files via upload

---
+++ 6 задача.cpp
@@ -0,0 +1,50 @@
+﻿#include <iostream>
+#include <vector>
+
+int main()
+{
+	//вводит вектор
+	std::vector<int> vec = { 1,2,3 };
+	for (int i = 0; i < vec.size(); i++)
+		std::cout << vec[i];
+	std::cout << std::endl;
+	//добавляет числа в конец (нужен объект типа элемента)
+	vec.push_back(2);
+	for (int i = 0; i < vec.size(); i++)
+		std::cout << vec[i];
+	std::cout << std::endl;
+	//вводим итератор, который указывает на первый элемент вектора
+	std::vector<int>::iterator iter = vec.begin(); 
+	//удаляет элемент по итератору
+	vec.erase(iter + 1);
+	for (int i = 0; i < vec.size(); i++)
+		std::cout << vec[i];
+	std::cout << std::endl;
+	//вставляет элемент на позицию, указанную итератором
+	vec.insert(iter + 2, 8);
+	for (int i = 0; i < vec.size(); i++)
+		std::cout << vec[i];
+	std::cout << std::endl;
+	//добавляет числа в конец (просто передаются элементы)
+	vec.emplace_back(7);
+	for (int i = 0; i < vec.size(); i++)
+		std::cout << vec[i];
+	std::cout << std::endl;
+	//меняет размер вектора
+	vec.resize(3);
+	for (int i = 0; i < vec.size(); i++)
+		std::cout << vec[i];
+	std::cout << std::endl;
+	//меняет объем выделенной памяти
+	vec.reserve(8);
+	//уменьшает кол-ко используемой памяти до размера вектора
+	vec.shrink_to_fit();
+	//возвращает объем памяти, выделенный для вектора
+	vec.capacity();
+	//удаляет все элементы
+	vec.clear();
+	//5 пункт задачи
+	std::vector<int> vec1 = { 1,2,3 };
+	vec = vec1;
+	vec[1] = 10;
+}
---
+++ задача 4.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+
+
+
+int main()
+{
+	int q1, p1, q2, p2, a;
+	std::cin >> q1 >> p1 >> q2 >> p2 >> a;
+	int sum = 0;
+	int t;
+	if (q1 > q2)
+		t = a / q2 + 1;
+	else
+		t = a / q2 + 1;
+	int min = 1000000000;
+	for (int i = 0; i < t; i++)
+		for (int j = 0; j < t; j++)
+			if (q1 * i + q2 * j >= a)
+			{
+				sum = p1 * i + p2 * j;
+				if (sum < min)
+					min = sum;
+			}
+	std::cout << min;
+}
